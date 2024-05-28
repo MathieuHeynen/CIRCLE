794c4d1b210c32c6c391ab32e0fde548b6c1e0c7
Mathieu Heynen <contact.heynen@gmail.com>
2024-05-28 16:53:01 +0200

    nesting the routes of rental like my buddy says <3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items, only: %i[show new create]
-  resources :rental, only: %i[show create]
+  resources :items, only: %i[show new create] do
+    resources :rental, only: :create
+  end
 end

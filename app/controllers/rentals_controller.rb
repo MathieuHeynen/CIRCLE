2a409012806abaab301750a54124c607e54cfb4a
Mathieu Heynen <contact.heynen@gmail.com>
2024-05-28 16:00:53 +0200

    creating the action of the rental controller, and the form in the rental directory in view

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,21 @@
 class RentalsController < ApplicationController
+  #creating a new clothe rental
+
+  def create
+    @item = Item.find(params[:item_id])
+    @rental = Rental.new(rental_params)
+    @rental.item = @item
+    @rental.user = current_user
+    if @rental.save
+      redirect_to item_path(@item)
+    else
+      render "items/show", notice: 'Something went wrong with your rent request', status: :unprocessable_entity
+    end
+  end
+
+  #-------PRIVATE METHODS-------
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date)
+  end
 end

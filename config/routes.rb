048fd48f58fe3056be5156a6f52ff3d7260d513d
Mathieu Heynen <contact.heynen@gmail.com>
2024-05-30 15:16:44 +0200

    upgraded all the links of the footer and the pages linked to them

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   get '/about_us', to: "pages#about_us"
   get '/contact_us', to: "pages#contact_us"
+  post '/contact_us', to: "pages#create"
   get '/terms_of_services', to: "pages#terms_of_services"
 end

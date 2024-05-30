ecb362eb7b17bdc09d340b9122ab46cb8d34fa45
Guillaume Verlooy <guillaumeverlooy@gmail.com>
2024-05-30 11:50:08 +0200

    Front-end grid is complete -> check with new description input for card heights?

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 require "open-uri"
-
 puts "Cleaning database..."
+Item.all.each {|item| item.photo.purge}
 Item.destroy_all
 User.destroy_all
 
@@ -19,20 +19,20 @@ user = User.create!(email: "test@test.com", password: "123456")
 
 puts "Creating items..."
 
-file = URI.open("https://plus.unsplash.com/premium_photo-1683143646131-92a439faca4d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-file_one = URI.open("https://plus.unsplash.com/premium_photo-1692441578418-88b76776791e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-file_two = URI.open("https://images.unsplash.com/photo-1493568000180-ca2fb70ddcba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8amFja2V0fGVufDB8MHwwfHx8MA%3D%3D")
-file_three = URI.open("https://plus.unsplash.com/premium_photo-1705554330163-2e0ccc1808e2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+file = URI.open("./app/assets/images/dress1.jpg")
+file_one = URI.open("./app/assets/images/dress2.jpg")
+file_two = URI.open("./app/assets/images/suit1.jpg")
+file_three = URI.open("./app/assets/images/suit2.jpg")
 
-trouser = Item.new(name: "trousers", brand: "COS", size: "M", price: "10", user: user)
-shirt = Item.new(name: "shirt", brand: "COS", size: "M", price: "10", user: user)
-jacket = Item.new(name: "jacket", brand: "COS", size: "M", price: "10", user: user)
-shoes = Item.new(name: "shoes", brand: "COS", size: "M", price: "10", user: user)
+trouser = Item.new(name: "Dress", brand: "COS", size: "M", price: "10", description: "Great piece of clothing, in immaculate state. Be the best dressed guest at your next event", user: user)
+shirt = Item.new(name: "Dress", brand: "COS", size: "M", price: "10", description: "Great piece of clothing, in immaculate state. Be the best dressed guest at your next event", user: user)
+jacket = Item.new(name: "Suit", brand: "COS", size: "M", price: "10", description: "Great piece of clothing, in immaculate state. Be the best dressed guest at your next event", user: user)
+shoes = Item.new(name: "Suit", brand: "COS", size: "M", price: "10", description: "Great piece of clothing, in immaculate state. Be the best dressed guest at your next event", user: user)
 
-trouser.photo.attach(io: file, filename: "trousers.jpg", content_type: "image/jpg")
-shirt.photo.attach(io: file_one, filename: "shirt.jpg", content_type: "image/jpg")
-jacket.photo.attach(io: file_two, filename: "jacket.jpg", content_type: "image/jpg")
-shoes.photo.attach(io: file_three, filename: "shoes.jpg", content_type: "image/jpg")
+trouser.photo.attach(io: file, filename: "dress1.jpg", content_type: "image/jpg")
+shirt.photo.attach(io: file_one, filename: "dress2.jpg", content_type: "image/jpg")
+jacket.photo.attach(io: file_two, filename: "suit1.jpg", content_type: "image/jpg")
+shoes.photo.attach(io: file_three, filename: "suit2.jpg", content_type: "image/jpg")
 
 trouser.save!
 shirt.save!

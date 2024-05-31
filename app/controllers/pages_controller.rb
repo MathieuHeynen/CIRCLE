1e8981a969ec25e43c6ef3735c2d8c4ce03e1fc7
aika balmukhanova <abalmukh@gmail.com>
2024-05-31 11:38:47 +0200

    authorised access to footer links without needing to be logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[about_us contact_us terms_of_services]
+  
   def create
     redirect_to root_path, notice: "Your message has been successfully sent ❤️"
   end
-
 end
